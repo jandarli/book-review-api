3ba450f0774ee0845d8323cd8d624b0f716990bf
jandarli <janicedarling1@gmail.com>
2025-05-26 16:43:24 -0400

    feat: Adding migration for users table

---
+++ migrations/1748291069661_create-users-table.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR NOT NULL,
+            username VARCHAR NOT NULL,
+            password VARCHAR NOT NULL CHECK (char_length(password) >= 8),  
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            UNIQUE (email)
+        );
+         `)
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE IF EXISTS create_users_table;
+        `)
+};
