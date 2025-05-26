9bb4a9e4e7c68f49f107bd8a4bd0f1945ab1f2c5
jandarli <janicedarling1@gmail.com>
2025-05-26 17:00:48 -0400

    fix: Updating migrations for users table

--- migrations/1748291069661_create-users-table.js
+++ migrations/1748291069661_create-users-table.js
@@ -16,7 +16,8 @@ exports.up = (pgm) => {
             username VARCHAR NOT NULL,
             password VARCHAR NOT NULL CHECK (char_length(password) >= 8),  
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            UNIQUE (email)
+            UNIQUE (email),
+            UNIQUE (username)
         );
          `)
 };
