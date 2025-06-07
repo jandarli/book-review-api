41ef3c26507243bc6590276be388dbc7eef8cba5
jandarli <janicedarling1@gmail.com>
2025-06-07 18:32:47 -0400

    feat: Adding migrations for user function

---
+++ migrations/1749331334316_get-user-function.js
@@ -0,0 +1,40 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.sql(`
+        DROP FUNCTION IF EXISTS get_user(VARCHAR);
+
+        CREATE OR REPLACE FUNCTION get_user(
+            IN p_username VARCHAR
+        )
+        RETURNS TABLE(id INT, username VARCHAR, email VARCHAR, password VARCHAR)
+        LANGUAGE plpgsql
+        AS $$
+        BEGIN
+            RETURN QUERY
+            SELECT users.id, users.username, users.email, users.password
+            FROM users
+            WHERE users.username = p_username;
+        END;
+        $$;
+        `)
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP FUNCTION IF EXISTS get_user(VARCHAR);
+        `)
+};
