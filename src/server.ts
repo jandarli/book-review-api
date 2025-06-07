bef1777859e6c0feccb7a1f73a6686515adb4602
jandarli <janicedarling1@gmail.com>
2025-06-07 18:32:18 -0400

    feat: Adding authentication routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import bookRoutes from './routes/bookRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express(); 
 const PORT = 4001;
 
 app.use(express.json());
 app.use('/api', bookRoutes);
+app.use('/auth', authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
