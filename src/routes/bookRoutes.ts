9f406c7d7e19aac56310fda3376adcecd8718fce
jandarli <janicedarling1@gmail.com>
2025-06-28 13:15:33 -0400

    fix: Adding authenticate logic to insert books

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getBooks, insertBooks, getBook} from '../controllers/bookControllers';
-
+import { authenticateJWT } from '../middleware/authMiddleware';
 const router = Router();
 
 router.get('/books', getBooks);
 router.get('/books/search', getBook);
-router.post('/books', insertBooks);
+router.post('/books', authenticateJWT, insertBooks);
 
 export default router;
