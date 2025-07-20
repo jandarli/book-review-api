f62199332c7ed399b881f7d097db66980b366825
jandarli <janicedarling1@gmail.com>
2025-07-20 14:22:15 -0400

    feat: Adding migration for new user table column

---
+++ migrations/1753031626744_add-roles-to-user-table.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createType('user_role', ['reader', 'library_admin', 'platform_admin']);
+    
+    pgm.addColumn('users', {
+        role: {
+            type: 'user_role',
+            notNull: true,
+            default: 'reader'
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropColumn('users', 'role');
+    pgm.dropType('user_role');
+};
