27edddbdd8aca4473af3b722f9f7cbc70cb7d682
jandarli <janicedarling1@gmail.com>
2025-07-20 14:22:50 -0400

    Adding migration for change to insert user stored procedure

---
+++ migrations/1753032424782_insert-user-with-role-procedure.js
@@ -0,0 +1,41 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.sql(`
+        DROP PROCEDURE IF EXISTS insert_user(VARCHAR, VARCHAR, VARCHAR, USER_ROLE);
+
+        CREATE OR REPLACE PROCEDURE insert_user(
+            IN p_username VARCHAR(255),
+            IN p_email VARCHAR(255),
+            IN p_password VARCHAR(255),
+            IN p_role USER_ROLE
+        )
+        LANGUAGE plpgsql
+        AS $$
+        BEGIN
+            INSERT INTO users(username, email, password, role)
+            VALUES (p_username, p_email, p_password, p_role);
+        END;
+        $$;
+
+        `)
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP PROCEDURE IF EXISTS insert_user(VARCHAR, VARCHAR, VARCHAR, USER_ROLE);
+        `)
+};
