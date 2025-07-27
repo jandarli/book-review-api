00270468fffc3b71e8284dcb6bdf30164df149a8
jandarli <janicedarling1@gmail.com>
2025-07-27 14:04:04 -0400

    feat: Adding role checks

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export interface AuthRequest extends Request {
-  user?: { id: number; username: string };
+  user?: { id: number; username: string; role: string };
 }
 
 
@@ -19,8 +19,9 @@ export const authenticateJWT = (req: AuthRequest, res: Response, next: NextFunct
 
     try {
         const secret = process.env.JWT_SECRET as string;
-        const decoded = jwt.verify(token, secret) as { id: number; username: string };
+        const decoded = jwt.verify(token, secret) as { id: number; username: string; role: string };
         req.user = decoded;
+        console.log(req.user);
         next();
 
     } catch (err) {
@@ -28,4 +29,25 @@ export const authenticateJWT = (req: AuthRequest, res: Response, next: NextFunct
         return;
     }
 
+}
+
+
+export const authorizeRoles = (...roles: string[] ) => {
+    return (req: AuthRequest, res: Response, next: NextFunction ) => {
+        if (!req.user || !req.user.role) {
+            console.error('User or user role not found on request after authentication.');
+            res.status(403).json({ error: 'Access Denied: User role not found' });
+            return;
+        }
+
+         if (roles.includes(req.user.role)) {
+            next();
+            return;
+
+        } else {
+            console.warn(`Access Denied for user ${req.user.username} with role ${req.user.role}. Required roles: ${roles.join(', ')}`);
+            res.status(403).json({ error: 'Access Denied: Insufficient permissions' });
+            return;
+        }
+    }
 }
