2a8ff79d0d79d670339f07f6548a72ff6687fe2c
jandarli <janicedarling1@gmail.com>
2025-08-10 17:13:39 -0400

    feat: Updating book routes with insert reviews endpoint

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getBooks, insertBooks, getBook} from '../controllers/bookControllers';
+import { getBooks, insertBooks, getBook, insertReview} from '../controllers/bookControllers';
 import { authenticateJWT, authorizeRoles } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.get('/books', getBooks);
 router.get('/books/search', getBook);
 router.post('/books', authenticateJWT, authorizeRoles('library_admin', 'platform_admin'), insertBooks);
+router.post('/books/:bookId/reviews', authenticateJWT, authorizeRoles('reader'), insertReview);
 
 export default router;
