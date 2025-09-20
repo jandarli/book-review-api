5b572b5bdb040a890927176b8dc6360eccce02ee
jandarli <janicedarling1@gmail.com>
2025-09-20 17:21:10 -0400

    format: Grouping endpoints by type

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import { getBooks, insertBooks, getBook, insertReview, getReviews} from '../controllers/bookControllers';
+import { getBooks, getBook, getReviews, insertBooks, insertReview } from '../controllers/bookControllers';
 import { authenticateJWT, authorizeRoles } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.get('/books', getBooks);
 router.get('/books/search', getBook);
+router.get('/books/reviews', getReviews);
+
 router.post('/books', authenticateJWT, authorizeRoles('library_admin', 'platform_admin'), insertBooks);
 router.post('/books/:bookId/reviews', authenticateJWT, authorizeRoles('reader'), insertReview);
-router.get('/books/reviews', getReviews);
+
 export default router;
