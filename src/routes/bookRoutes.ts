58da57732538deccca54c19b7e889f2b7b3664ce
jandarli <janicedarling1@gmail.com>
2025-09-20 17:17:31 -0400

    feat: Adding get reviews route

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getBooks, insertBooks, getBook, insertReview} from '../controllers/bookControllers';
+import { getBooks, insertBooks, getBook, insertReview, getReviews} from '../controllers/bookControllers';
 import { authenticateJWT, authorizeRoles } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -8,5 +8,5 @@ router.get('/books', getBooks);
 router.get('/books/search', getBook);
 router.post('/books', authenticateJWT, authorizeRoles('library_admin', 'platform_admin'), insertBooks);
 router.post('/books/:bookId/reviews', authenticateJWT, authorizeRoles('reader'), insertReview);
-
+router.get('/books/reviews', getReviews);
 export default router;
